c3008b81f40d4852f63a98b15dc393a29d55557a
Miguel Parkour <miguelparkour93@gmail.com>
2025-01-04 17:51:57 +0100

    Ajuste de base en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/BuscaminasSensual/',
+  base: '/BuscaminasSensual/', // Cambia esto por el nombre exacto de tu repositorio
   plugins: [react()],
 });
