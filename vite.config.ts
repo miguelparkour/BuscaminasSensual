f640df1c01603f6e3daba642a0378acc1e3fe6a0
Miguel Parkour <miguelparkour93@gmail.com>
2025-01-04 18:18:27 +0100

    test23

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/BuscaminasSensual/', // Cambia esto por el nombre exacto de tu repositorio
+  base: './', // Cambia esto por el nombre exacto de tu repositorio
   plugins: [react()],
 });
